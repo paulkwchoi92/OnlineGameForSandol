ac1050e0c10cf12f9c92a301cf92c5f6735ee9cb
paulkwchoi92 <paulkwchoi92@gmail.com>
2020-05-06 16:48:28 -0700

    added react hook event listners

--- src/components/character/index.jsx
+++ src/components/character/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
 export default function Character() {
-  const offset = { top: 0, left: 0}
+  //increment left by 32 to swithc off between characters
+  // girl character = 96
+  const offset = { top: 0, left: 0 };
   return (
     <div
-      style={{ width: 32, height: 32, background: `url(/characters.prng) -${offset.left} -${offset.top}` }}
+      style={{
+        width: 32,
+        height: 32,
+        //url will automatically looking into public folder
+        background: `url(/characters.png) -${offset.left} -${offset.top}`,
+      }}
     />
   );
 }
