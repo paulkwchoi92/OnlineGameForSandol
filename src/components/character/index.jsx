fa21b3c2f936db73e4a722a2cc6420dc31f5681f
paulkwchoi92 <paulkwchoi92@gmail.com>
2020-05-06 16:55:45 -0700

    adding event listners to arrows

--- src/components/character/index.jsx
+++ src/components/character/index.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-
+import useEventListener from '@user-it/event-Listner'
 export default function Character() {
   //increment left by 32 to swithc off between characters
   // girl character = 96
   const offset = { top: 0, left: 0 };
+  useEventListener("keydown", ({ code }) => {
+    // equal to negative one also indiacted if the string was not found
+    if (code.indexOf("Arrow") === -1) return 
+    // code returns ArrowDown, ArrowUp and suche, replacing the Arrow will shorten it down
+    const direction = code.replace("Arrow", "")
+  })
   return (
     <div
       style={{
