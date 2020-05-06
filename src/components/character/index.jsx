0732d4af1d66a35ec97ec382c04212907748a48d
paulkwchoi92 <paulkwchoi92@gmail.com>
2020-05-06 16:56:12 -0700

    DRYING up

--- src/components/character/index.jsx
+++ src/components/character/index.jsx
@@ -4,6 +4,7 @@ export default function Character() {
   //increment left by 32 to swithc off between characters
   // girl character = 96
   const offset = { top: 0, left: 0 };
+  const SIZE = 32
   useEventListener("keydown", ({ code }) => {
     // equal to negative one also indiacted if the string was not found
     if (code.indexOf("Arrow") === -1) return 
@@ -13,8 +14,8 @@ export default function Character() {
   return (
     <div
       style={{
-        width: 32,
-        height: 32,
+        width: SIZE,
+        height: SIZE,
         //url will automatically looking into public folder
         background: `url(/characters.png) -${offset.left} -${offset.top}`,
       }}
