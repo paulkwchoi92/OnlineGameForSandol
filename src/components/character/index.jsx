09c2f7b75d8ace183283536fc37ce3638e613d05
paulkwchoi92 <paulkwchoi92@gmail.com>
2020-05-06 16:59:31 -0700

    incorporating react hooks

--- src/components/character/index.jsx
+++ src/components/character/index.jsx
@@ -1,16 +1,25 @@
-import React from "react";
-import useEventListener from '@user-it/event-Listner'
+import React, { useState } from "react";
+import useEventListener from "@user-it/event-Listner";
+const SIZE = 32;
+// under code will repalce offset of top to change character visualization
+const DIRECTION = {
+  DOWN: 0,
+  LEFT: SIZE * 1,
+  RIGHT: SIZE * 2,
+  UP: SIZE * 3,
+};
 export default function Character() {
   //increment left by 32 to swithc off between characters
   // girl character = 96
   const offset = { top: 0, left: 0 };
-  const SIZE = 32
+  //default of character as facing down
+  const [facing, setFacing] = useState(DIRECTION.DOWN);
   useEventListener("keydown", ({ code }) => {
     // equal to negative one also indiacted if the string was not found
-    if (code.indexOf("Arrow") === -1) return 
+    if (code.indexOf("Arrow") === -1) return;
     // code returns ArrowDown, ArrowUp and suche, replacing the Arrow will shorten it down
-    const direction = code.replace("Arrow", "")
-  })
+    const direction = code.replace("Arrow", "");
+  });
   return (
     <div
       style={{
