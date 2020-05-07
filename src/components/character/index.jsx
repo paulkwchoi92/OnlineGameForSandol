e0ce5ead11956691d9ecd8c6f310a5f76e068a13
paulkwchoi92 <paulkwchoi92@gmail.com>
2020-05-06 17:10:24 -0700

    adding character steps

--- src/components/character/index.jsx
+++ src/components/character/index.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import useEventListener from "@user-it/event-Listner";
+import React, { useState, useEffect } from "react";
+import useEventListener from "@use-it/event-listener";
 const SIZE = 32;
 // under code will repalce offset of top to change character visualization
 const DIRECTION = {
@@ -8,25 +8,34 @@ const DIRECTION = {
   RIGHT: SIZE * 2,
   UP: SIZE * 3,
 };
+
+const STEP_SIZE = 3
 export default function Character() {
   //increment left by 32 to swithc off between characters
   // girl character = 96
   const offset = { top: 0, left: 0 };
   //default of character as facing down
-  const [facing, setFacing] = useState(DIRECTION.DOWN);
+  const [facing, setFacing] = useState(DIRECTION.DOWN)
+  const [step, setStep] = useState(0)
   useEventListener("keydown", ({ code }) => {
     // equal to negative one also indiacted if the string was not found
     if (code.indexOf("Arrow") === -1) return;
     // code returns ArrowDown, ArrowUp and suche, replacing the Arrow will shorten it down
-    const direction = code.replace("Arrow", "");
+    const direction = DIRECTION[code.replace("Arrow", "").toUpperCase()];
+    setFacing(direction)
   });
+  //user effect happens when an piece of state changes
+  // the code underneath will be watching when the state facing will change
+  useEffect(() => {
+
+  }, [facing])
   return (
     <div
       style={{
         width: SIZE,
         height: SIZE,
         //url will automatically looking into public folder
-        background: `url(/characters.png) -${offset.left} -${offset.top}`,
+        background: `url(/characters.png) -${offset.left}px -${offset.top + facing}px`,
       }}
     />
   );
